9eaac737a2e4a5deacb1cc6c667ddf42d5179c5e
Imnotndesh <slateinbox@gmail.com>
2023-04-28 11:25:26 +0300

    Added Lyric Display

--- lyricsfetch.py
+++ lyricsfetch.py
@@ -1,9 +1,11 @@
 import azapi
 action = 0
 API = azapi.AZlyrics('google', accuracy=0.5)
-
 def fetcher(artist):
     API.artist = artist
     API.getLyrics(save=True, ext='lrc')
-    print(f"{API.lyrics}\n")
-    print(f"{API.title} by {API.artist}")
+    global fetchedLyrics
+    global musicDetails
+    fetchedLyrics =(f"{API.lyrics}\n")
+    musicDetails = (f"{API.title} by {API.artist}")
+    return fetchedLyrics,musicDetails
